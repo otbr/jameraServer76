80426910afb3e6b22438077d7ebbdd1758964ce3
Bruno de Deus <Bruno de Deus>
2017-04-14 13:50:38 -0300

    Alterado quest mensagem rook

--- data/actions/scripts/rook/rLeatherArmor.lua
+++ data/actions/scripts/rook/rLeatherArmor.lua
@@ -2,7 +2,7 @@ function onUse(cid, item, frompos, item2, topos)
 if item.uid == 3037 then
   queststatus = getPlayerStorageValue(cid,3037)
   if queststatus == -1 or queststatus == 0 then
-   doPlayerSendTextMessage(cid,22,"You have found a torch.")
+   doPlayerSendTextMessage(cid,22,"You have found a reinforced leather armor.")
    item_uid = doPlayerAddItem(cid,5178,1)
    setPlayerStorageValue(cid,3037,1)
 
