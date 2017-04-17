0643cb2890dbe88c6570cae82505f4683e236284
Bruno de Deus <Bruno de Deus>
2017-04-17 19:53:37 -0300

    adicionado tabs

--- data/actions/scripts/thais/inicial_set.lua
+++ data/actions/scripts/thais/inicial_set.lua
@@ -3,15 +3,10 @@ if item.uid == 4019 then
   queststatus = getPlayerStorageValue(cid,4019)
   if queststatus == -1 and getPlayerAccess(cid) == 0 then
    doPlayerSendTextMessage(cid,22,"You have found a bag.")
-   bag = doPlayerAddItem(cid,1988,1)
-	doAddContainerItem(bag,5180,1)
-	doAddContainerItem(bag,5181,1)
-	doAddContainerItem(bag,5178,1)
-	doAddContainerItem(bag,5179,1)
-	doAddContainerItem(bag,5158,1)
-	doAddContainerItem(bag,2394,1)
-	doAddContainerItem(bag,2423,1)
-	doAddContainerItem(bag,2383,1)
+   bag = doPlayerAddItem(cid,1987,1)
+   doAddContainerItem(bag,2143,1)
+   doAddContainerItem(bag,2144,1)
+   doAddContainerItem(bag,2148,100)
    setPlayerStorageValue(cid,4019,1)
   else
    doPlayerSendTextMessage(cid,22,"The box is empty.")
@@ -21,6 +16,4 @@ else
 end
 return 1
 end
-
-
 
