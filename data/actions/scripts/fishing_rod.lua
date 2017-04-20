19318112745eff99dd1fcc1ee6465023b6e79b65
Bruno de Deus <Bruno de Deus>
2017-04-20 20:04:32 -0300

    ajustado erro da vara

--- data/actions/scripts/fishing_rod.lua
+++ data/actions/scripts/fishing_rod.lua
@@ -12,9 +12,10 @@ local waterIds = {493, 4608, 4609, 4610, 4611, 4612, 4613, 4614, 4615, 4616, 461
 function onUse(cid, item, frompos, item2, topos)
         if isInArray(waterIds, item2.itemid) == TRUE then
                 if item2.itemid ~= 493 then
-                        if useWorms == FALSE or useWorms == TRUE and doPlayerRemoveItem(cid, ITEM_WORM, 1) == TRUE then
+                        if useWorms == FALSE or useWorms == TRUE then
                                 if math.random(1,(100+(getPlayerSkill(cid, SKILL_FISHING)/10))) <= getPlayerSkill(cid, SKILL_FISHING) then
                                     doPlayerAddItem(cid, ITEM_FISH, 1)
+									doPlayerRemoveItem(cid, ITEM_WORM, 1)
                                 end
                                 doPlayerAddSkillTry(cid, SKILL_FISHING, 1)
                         end
