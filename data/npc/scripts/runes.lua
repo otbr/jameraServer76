2047450770ec47ed08e0dfc1192ff534190e8e9b
Bruno de Deus <Bruno de Deus>
2017-05-06 18:50:23 -0300

    alteracoes npc runes

--- data/npc/scripts/runes.lua
+++ data/npc/scripts/runes.lua
@@ -232,66 +232,66 @@ function creatureSayCallback(cid, type, msg)
 	end	
 	
 		return TRUE
-end 
 
-local shopModule = ShopModule:new()
-npcHandler:addModule(shopModule)
 
--- Empty Vial
-shopModule:addSellableItem({'empty', 'vial'}, 2006, 5, 'vial')
+	local shopModule = ShopModule:new()
+	npcHandler:addModule(shopModule)
 
--- Spell Book
+	-- Empty Vial
+	shopModule:addSellableItem({'empty', 'vial'}, 2006, 5, 'vial')
 
-shopModule:addBuyableItem({'spellbook'}, 2175, 150,	'spellbook')
+	-- Spell Book
 
--- Wand
+	shopModule:addBuyableItem({'spellbook'}, 2175, 150,	'spellbook')
 
-shopModule:addBuyableItem({'light wand', 'lightwand'}, 2163, 500, 'magic light wand')
-shopModule:addBuyableItem({'wand of vortex', 'vortex'}, 2190, 0, 'wand of vortex')
-shopModule:addBuyableItem({'wand of dragonbreath', 'dragonbreath'}, 2191, 1000, 'wand of dragonbreath')
-shopModule:addBuyableItem({'wand of plague', 'plague'}, 2188, 5000, 'wand of plague')
-shopModule:addBuyableItem({'wand of cosmic energy', 'cosmic energy'}, 2189, 10000, 'wand of cosmic energy')
-shopModule:addBuyableItem({'wand of inferno', 'inferno'}, 2187, 15000, 'wand of inferno')
+	-- Wand
 
--- Rods
+	shopModule:addBuyableItem({'light wand', 'lightwand'}, 2163, 500, 'magic light wand')
+	shopModule:addBuyableItem({'wand of vortex', 'vortex'}, 2190, 0, 'wand of vortex')
+	shopModule:addBuyableItem({'wand of dragonbreath', 'dragonbreath'}, 2191, 1000, 'wand of dragonbreath')
+	shopModule:addBuyableItem({'wand of plague', 'plague'}, 2188, 5000, 'wand of plague')
+	shopModule:addBuyableItem({'wand of cosmic energy', 'cosmic energy'}, 2189, 10000, 'wand of cosmic energy')
+	shopModule:addBuyableItem({'wand of inferno', 'inferno'}, 2187, 15000, 'wand of inferno')
 
-shopModule:addBuyableItem({'snakebite rod', 'snakebite'}, 2182, 0, 'snakebite rod')
-shopModule:addBuyableItem({'moonlight rod', 'moonlight'}, 2186, 1000, 'moonlight rod')
-shopModule:addBuyableItem({'volcanic rod', 'volcanic'}, 2185, 5000, 'necrotic rod')
-shopModule:addBuyableItem({'terra rod', 'terra'}, 2181, 10000, 'terra rod')
-shopModule:addBuyableItem({'tempest rod', 'tempest'}, 2183, 15000, 'tempest rod')
+	-- Rods
 
--- Fluids
+	shopModule:addBuyableItem({'snakebite rod', 'snakebite'}, 2182, 0, 'snakebite rod')
+	shopModule:addBuyableItem({'moonlight rod', 'moonlight'}, 2186, 1000, 'moonlight rod')
+	shopModule:addBuyableItem({'volcanic rod', 'volcanic'}, 2185, 5000, 'necrotic rod')
+	shopModule:addBuyableItem({'terra rod', 'terra'}, 2181, 10000, 'terra rod')
+	shopModule:addBuyableItem({'tempest rod', 'tempest'}, 2183, 15000, 'tempest rod')
 
-shopModule:addBuyableItem({'life fluid','lifefluid'}, 2006, 60,10,'life fluid')
-shopModule:addBuyableItem({'mana fluid','manafluid'},2006,55,7,'mana fluid')
+	-- Fluids
 
--- Runas 
+	shopModule:addBuyableItem({'life fluid','lifefluid'}, 2006, 60,10,'life fluid')
+	shopModule:addBuyableItem({'mana fluid','manafluid'},2006,55,7,'mana fluid')
 
-shopModule:addBuyableItem({'intense healing rune', 'ih'}, 2265, 95, 1, 'intense healing rune')
-shopModule:addBuyableItem({'ultimate healing rune', 'uh'}, 2273, 125, 1, 'ultimate healing rune')
-shopModule:addBuyableItem({'magic wall rune'}, 2293, 200, 3, 'magic wall rune')
-shopModule:addBuyableItem({'destroy field rune'}, 2261, 45, 3, 'destroy field rune')
-shopModule:addBuyableItem({'light magic missile rune'}, 2287, 40, 10, 'light magic missile rune')
-shopModule:addBuyableItem({'heavy magic missile rune', 'hmm'}, 2311, 50, 5, 'heavy magic missile rune')
-shopModule:addBuyableItem({'great fireball rune', 'gfb'}, 2304, 100, 4, 'great fireball rune')
-shopModule:addBuyableItem({'explosion rune', 'explosion'}, 2313, 150, 3, 'explosion rune')
-shopModule:addBuyableItem({'sudden death rune', 'sd'}, 2268, 225, 1, 'sudden death rune')
-shopModule:addBuyableItem({'death arrow rune'}, 2263, 300, 3, 'death arrow rune')
-shopModule:addBuyableItem({'paralyze rune'}, 2278, 700, 1, 'paralyze rune')
-shopModule:addBuyableItem({'animate dead rune'}, 2316, 375, 1, 'animate dead rune')
-shopModule:addBuyableItem({'convince creature rune'}, 2290, 80, 1, 'convince creature rune')
-shopModule:addBuyableItem({'chameleon rune'}, 2291, 210, 1, 'chameleon rune')
-shopModule:addBuyableItem({'desintegrate rune'}, 2310, 80,  3, 'desintegreate rune')
-shopModule:addBuyableItem({'fire bomb rune', 'fb'},2305, 117,'fire bomb rune')
-shopModule:addBuyableItem({'energy field rune', 'ef'},2277, 117,'energy field rune')
-shopModule:addBuyableItem({'poison wall rune', 'pw'},2289, 52,'poison wall rune')
-shopModule:addBuyableItem({'fire field rune', 'ff'},2301, 52,'fire field rune')
-shopModule:addBuyableItem({'energy bomb rune'},2262, 162,'fire field rune')
-shopModule:addBuyableItem({'adeta sio rune', 'as'},2262, 52,'fire field rune')
-shopModule:addBuyableItem({'poison bomb rune', 'pb'},2286, 162,'poison bomb rune')
-shopModule:addBuyableItem({'fire wall rune', 'fw'},2303, 52,'fire wall rune')
-shopModule:addBuyableItem({'blank', 'rune'},2260, 10,'blank rune')
+	-- Runas 
+
+	shopModule:addBuyableItem({'intense healing rune', 'ih'}, 2265, 95, 1, 'intense healing rune')
+	shopModule:addBuyableItem({'ultimate healing rune', 'uh'}, 2273, 125, 1, 'ultimate healing rune')
+	shopModule:addBuyableItem({'magic wall rune'}, 2293, 200, 3, 'magic wall rune')
+	shopModule:addBuyableItem({'destroy field rune'}, 2261, 45, 3, 'destroy field rune')
+	shopModule:addBuyableItem({'light magic missile rune'}, 2287, 40, 10, 'light magic missile rune')
+	shopModule:addBuyableItem({'heavy magic missile rune', 'hmm'}, 2311, 50, 5, 'heavy magic missile rune')
+	shopModule:addBuyableItem({'great fireball rune', 'gfb'}, 2304, 100, 4, 'great fireball rune')
+	shopModule:addBuyableItem({'explosion rune', 'explosion'}, 2313, 150, 3, 'explosion rune')
+	shopModule:addBuyableItem({'sudden death rune', 'sd'}, 2268, 225, 1, 'sudden death rune')
+	shopModule:addBuyableItem({'death arrow rune'}, 2263, 300, 3, 'death arrow rune')
+	shopModule:addBuyableItem({'paralyze rune'}, 2278, 700, 1, 'paralyze rune')
+	shopModule:addBuyableItem({'animate dead rune'}, 2316, 375, 1, 'animate dead rune')
+	shopModule:addBuyableItem({'convince creature rune'}, 2290, 80, 1, 'convince creature rune')
+	shopModule:addBuyableItem({'chameleon rune'}, 2291, 210, 1, 'chameleon rune')
+	shopModule:addBuyableItem({'desintegrate rune'}, 2310, 80,  3, 'desintegreate rune')
+	shopModule:addBuyableItem({'fire bomb rune', 'fb'},2305, 117,'fire bomb rune')
+	shopModule:addBuyableItem({'energy field rune', 'ef'},2277, 117,'energy field rune')
+	shopModule:addBuyableItem({'poison wall rune', 'pw'},2289, 52,'poison wall rune')
+	shopModule:addBuyableItem({'fire field rune', 'ff'},2301, 52,'fire field rune')
+	shopModule:addBuyableItem({'energy bomb rune'},2262, 162,'fire field rune')
+	shopModule:addBuyableItem({'adeta sio rune', 'as'},2262, 52,'fire field rune')
+	shopModule:addBuyableItem({'poison bomb rune', 'pb'},2286, 162,'poison bomb rune')
+	shopModule:addBuyableItem({'fire wall rune', 'fw'},2303, 52,'fire wall rune')
+	shopModule:addBuyableItem({'blank', 'rune'},2260, 10,'blank rune')
 end
 
 npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
